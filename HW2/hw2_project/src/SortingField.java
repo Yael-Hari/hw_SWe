2ed8368f2c937f6e6e0f81c1a632b1180e6e5876
Dan <dan.amler@gmail.com>
2021-06-17 14:55:16 +0300

    ff

--- HW2/hw2_project/src/SortingField.java
+++ HW2/hw2_project/src/SortingField.java
@@ -1,2 +1,9 @@
-public class SortingField {
+enum SortingField
+{
+    sort_by_name;
+    sort_by_size;
+    sort_by_process;
 }
+
+
+
--- HW2/hw2_project/src/StorageItem.java
+++ HW2/hw2_project/src/StorageItem.java
@@ -1,2 +1,29 @@
-public class StorageItem {
+abstract class StorageItem {
+    private String[] name;
+    private int date;
+
+    public StorageItem(String[] name){
+        /**builder**/
+        this.name = name;
+    }
+    public int getName() {
+        return this.number;
+    }
+
+    public int getDate() {
+        return this.number;
+    }
+
+    public void setName(String[] new_name){
+        this.name = new_name;
+    }
+
+
+
+
+
+
+
+
+
 }
