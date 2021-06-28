53e1d70454af89c9c86ecdcdc455692417577731
YaelHari <yaelhhari@gmail.com>
2021-06-28 18:26:32 +0300

    4

--- HW3/hw3_project/src/Zoo.java
+++ HW3/hw3_project/src/Zoo.java
@@ -1,2 +1,32 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Zoo {
-    List<Folder> folderList;
+    private List<AnimalFactory> animalsList;
+    private List<Observer> observersList;
+    public static Zoo instance = null;
+
+    private Zoo(){
+        animalsList = new ArrayList<>();
+        observersList = new ArrayList<>();
+    }
+
+    public Zoo getInstance(){
+        if (instance == null) {
+            instance = new Zoo();
+        }
+        return instance;
+    }
+
+    public <T extends AnimalFactory> void addAnimal(T newAnimal){
+        this.animalsList.add(newAnimal);
+    }
+
+    public void showAnimalsInfo(){
+        //todo printing code
+    }
+
+    public void addObserver(Observer observer){
+        this.observersList.add(observer);
+    }
+}
--- HW3/hw3_project/src/ZooObserver.java
+++ HW3/hw3_project/src/ZooObserver.java
@@ -1,2 +1,16 @@
-public class ZooObserver {
+public class ZooObserver implements Observer {
+    private String name;
+
+    public ZooObserver(String name){
+        this.name = name;
+    }
+
+    @Override
+    public void update(String newUpdate){
+
+    }
+
+    public String getName() {
+        return this.name;
+    }
 }
